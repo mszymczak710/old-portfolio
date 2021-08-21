2051004ee606261185ec4761ddb8b789f9bf9c89
mszymczak710 <84203463+mszymczak710@users.noreply.github.com>
2021-08-21 21:37:38 +0200

    Delete javascript directory

--- javascript/last_updated.js
+++
@@ -1,9 +0,0 @@
-var xhttp=new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("last_updated").innerHTML =
-    this.getResponseHeader("Last-Modified");
-  }
-};
-xhttp.open("GET", "last_updated.txt", true);
-xhttp.send();
